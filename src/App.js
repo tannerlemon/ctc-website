015406ac61209ef95e2803ab5056aa3e9a9b4416
Tan <tannerlemon@Tanners-MacBook-Pro.local>
2021-03-13 00:04:54 -0500

    halfway animated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import { useState } from "react";
 import "./App.css";
-import Navigation from "./components/Navigation";
 import Slider from "./components/Slider";
 import background from "./imgs/Website-Together.jpg";
 
 function App() {
-  const [showOverlay, setShowOverlay] = useState(false);
-  const handleShowOverlay = () => {
-    setShowOverlay(true);
-  };
-  const handleCloseOverlay = () => {
-    setShowOverlay(false);
-  };
-
   return (
     <div
       className="pageWrapper"
       style={{ backgroundImage: `url(${background})` }}
     >
-      <Navigation
-        showOverlay={showOverlay}
-        handleShowOverlay={handleShowOverlay}
-        handleCloseOverlay={handleCloseOverlay}
-      />
-      {
-        <Slider
-          showOverlay={showOverlay}
-          handleCloseOverlay={handleCloseOverlay}
-        />
-      }
+      {<Slider />}
     </div>
   );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,35 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Slider.css";
 import { interpolate, interpolateTransformSvg } from "d3-interpolate";
 import { easeBackOut, easeBackInOut } from "d3-ease";
 import { Animate } from "react-move";
 import About from "./About";
 import cancelIcon from "../imgs/cancel.png";
+import Navigation from "./Navigation";
 
-export default function Slider({ showOverlay, handleCloseOverlay }) {
+export default function Slider() {
+  const [showOverlay, setShowOverlay] = useState(false);
+  const handleShowOverlay = () => {
+    setShowOverlay(true);
+  };
+  const handleCloseOverlay = () => {
+    setShowOverlay(false);
+  };
+
   return (
     <div style={{ width: "100%" }}>
+      <Navigation handleShowOverlay={handleShowOverlay} />
       <Animate
         show={showOverlay}
         start={() => ({
-          bottom: "-150vh",
+          bottom: "-100vh",
         })}
         enter={() => [
           {
-            bottom: "0",
-            backgroundColor: "#ffffff",
-            timing: { delay: 700, duration: 700, ease: easeBackOut },
+            bottom: ["0"],
+            timing: { duration: 1000, ease: easeBackOut },
           },
         ]}
         update={() => ({
-          bottom: "0",
-          backgroundColor: "#000000",
-          timing: { duration: 700, ease: easeBackOut },
+          bottom: ["0"],
+          timing: { duration: 1000, ease: easeBackOut },
         })}
         leave={() => [
           {
-            bottom: "-150vh",
-            timing: { duration: 750, ease: easeBackInOut },
+            bottom: ["-100vh"],
+            timing: { duration: 1050, ease: easeBackInOut },
           },
         ]}
         interpolation={(begValue, endValue, attr) => {
